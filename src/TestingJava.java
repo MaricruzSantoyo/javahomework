b1360552654cbf249416e3295f0b0f61c28456fa
Maricruz Santoyo <msantoy3@asu.edu>
2023-01-13 23:04:08 -0700

    adding files

---
+++ src/TestingJava.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class TestingJava {
+
+	public static void main(String[] args) {
+		
+		int num=1, word="hello";
+		
+		System.out.println(num);
+		
+		String number = 1;
+		
+		byte a = "";
+		
+		System.out.print(a);
+		
+		boolean b = "world";
+		
+		
+	}
+
+}
